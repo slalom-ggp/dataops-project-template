7a3e879b23a9887ad1db149ce5e7a141c416d96c
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-01-11 05:56:19 +0000

    gradle extracts

--- data/taps/install.sh
+++ data/taps/install.sh
@@ -3,6 +3,19 @@
 # Use this script to install any taps and targets needed for this project.
 # i.e.: s-tap install PLUGIN_NAME [ INSTALL_SOURCE [ PLUGIN_ALIAS ] ]
 
+set -ve
+
+# Uninstall if needed
+
+set +e
+
+rm /usr/bin/tap-salesforce >& /dev/null
+rm /usr/bin/tap-pardot >& /dev/null
+rm /usr/bin/target-csv >& /dev/null
+rm /usr/bin/target-s3-csv >& /dev/null
+
+set -e
+
 # Install taps:
 s-tap install tap-salesforce git+https://gitlab.com/meltano/tap-salesforce.git@master
 s-tap install tap-pardot
