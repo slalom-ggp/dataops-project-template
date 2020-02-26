d3fbdf72509b0a7bca76c8d9bd371db99cbae7df
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-02-25 17:11:31 -0800

    refactor using environment, add taps and dbt

---
+++ infra/01_secrets-upload.tf
@@ -0,0 +1,24 @@
+module "secrets" {
+  # BOILERPLATE HEADER (NO NEED TO CHANGE):
+  # TODO: Revert to stable source
+  # source        = "git::https://github.com/slalom-ggp/dataops-infra.git//catalog/aws/singer-taps?ref=master"
+  source                   = "../../dataops-infra/components/aws/secrets-manager"
+  name_prefix              = local.name_prefix
+  resource_tags            = local.project_tags
+  # environment              = module.env.environment
+  secrets_source_file_path = local.secrets_file_path
+
+  # ADD OR MODIFY CONFIGURATION HERE:
+
+  # secrets_names = [
+  #   "APPLICATION_A",
+  #   "APPLICATION_B",
+  #   "SIMPLE_SECRET"
+  # ]
+
+  /* OPTIONALLY, COPY-PASTE ADDITIONAL SETTINGS FROM BELOW:
+
+  kms_key_id = null # when null, use the default KMS key
+
+  */
+}
---
+++ infra/04_data-build-tool.tf
@@ -0,0 +1,34 @@
+module "dbt_on_aws" {
+
+  # BOILERPLATE HEADER (NO NEED TO CHANGE):
+  # TODO: Revert to stable source
+  # source        = "git::https://github.com/slalom-ggp/dataops-infra.git//catalog/aws/dbt?ref=master"
+  source          = "../../dataops-infra/catalog/aws/dbt"
+  name_prefix     = local.name_prefix
+  resource_tags   = local.project_tags
+  environment     = module.env.environment
+
+  # ADD OR MODIFY CONFIGURATION HERE:
+
+  container_image            = "slalomggp/dataops:test-project-latest-dev"
+  dbt_run_command            = "./gradlew dbtSeed dbtCompile dbtRun"
+  scheduled_timezone         = "PST"
+  scheduled_refresh_interval = "4 hours"
+  environment_vars = {
+    "PROJECT_GIT_URL" = "git+https://github.com/slalom-ggp/dataops-infra.git"
+    "WITH_SPARK"      = "true"
+  }
+
+  /* OPTIONALLY, COPY-PASTE ADDITIONAL SETTINGS FROM BELOW:
+
+  docker_registry_url        = ""
+  docker_user                = ""
+  docker_password            = ""
+  scheduled_refresh_interval = "1 minute"
+  scheduled_refresh_times    = ["0300", "1200", "1800"]
+
+  */
+}
+
+# BOILERPLATE OUTPUT (NO NEED TO CHANGE):
+output "dbt_summary" { value = module.dbt_on_aws.summary }
